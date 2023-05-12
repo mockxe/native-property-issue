fbc99c3348958c6d796429c54907bb1991dd351f
mockxe <38732499+mockxe@users.noreply.github.com>
2023-05-12 23:02:18 +0200

    added config properties unit test

--- src/test/java/io/mockxe/nativepropertyissue/NativePropertyIssueApplicationTests.java
+++ src/test/java/io/mockxe/nativepropertyissue/NativePropertyIssueApplicationTests.java
@@ -1,13 +1,23 @@
 package io.mockxe.nativepropertyissue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 
 @SpringBootTest
 class NativePropertyIssueApplicationTests {
 
+	@Autowired
+	ConfigProps configProps;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void propertyBinding() {
+		assert configProps.getMediaType().equals(MediaType.APPLICATION_JSON);
 	}
 
 }
