7086aa6fd2a5b5f3a281512605168e53a2b5ba3a
mockxe <38732499+mockxe@users.noreply.github.com>
2023-05-12 22:11:50 +0200

    Initial commit 🍃

---
+++ src/main/java/io/mockxe/nativepropertyissue/NativePropertyIssueApplication.java
@@ -0,0 +1,13 @@
+package io.mockxe.nativepropertyissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativePropertyIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativePropertyIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mockxe/nativepropertyissue/NativePropertyIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mockxe.nativepropertyissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativePropertyIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
